0f6f5fc1610cdf8c5ecfaa1787ba463a02312044
El dano <danielpliego@gmail.com>
2021-07-11 14:00:45 -0500

    comment out stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,14 +69,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Price"],
-        secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
-        downloadFiles: false,
-      },
-    },
-    {
       resolve: `gatsby-plugin-postcss`,
     },
   ],
