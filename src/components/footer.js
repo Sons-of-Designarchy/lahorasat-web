e249ae9dc4621e76cd6ee17acd6cb13ec86e74d3
El dano <danielpliego@gmail.com>
2021-08-26 15:17:27 -0600

    agregar nuevas redes y bloquear curso

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "../images/logo-footer.svg";
 import Instagram from "../images/ig.svg";
 import Twitter from "../images/twitter.svg";
+import Facebook from "../images/fb-logo.svg";
+import Youtube from "../images/youtube-icon.svg";
+import Linkedin from "../images/linkedin-icon.svg";
 
 const Footer = () => {
   return (
@@ -32,6 +35,27 @@ const Footer = () => {
           href="https://twitter.com/lahorasat"
         >
           <img src={Twitter} />
+        </a>
+        <a
+          target="_blank"
+          className="footer-social-link"
+          href="https://www.facebook.com/profile.php?id=100063771390136"
+        >
+          <img src={Facebook} />
+        </a>
+        <a
+          target="_blank"
+          className="footer-social-link"
+          href="https://www.youtube.com/channel/UC4oXv1xgGoPSxiFni7xVUWQ"
+        >
+          <img src={Youtube} />
+        </a>
+        <a
+          target="_blank"
+          className="footer-social-link"
+          href="https://www.linkedin.com/company/79887276"
+        >
+          <img src={Linkedin} />
         </a>
       </div>
       © 2021 La Hora Sat. Todos los derechos reservados.
