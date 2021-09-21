8e6a4f5614890af353b92d2f55939f24349da91e
El dano <danielpliego@gmail.com>
2021-09-20 21:11:45 -0500

    try again

--- src/pages/taller-basico.js
+++ src/pages/taller-basico.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import BannerCursos from "../components/banner-cursos";
 import Asesorias from "../components/asesorias";
 import BannerTallerBasico from "../components/banner-taller-basico";
 
