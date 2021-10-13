da145b0afcc6a0b5698ec2b9e4566953ebd0fea3
El dano <danielpliego@gmail.com>
2021-10-12 19:46:37 -0500

    add curso en video

---
+++ src/components/curso/header-curso.js
@@ -0,0 +1,63 @@
+// import { Link } from "gatsby"
+import React from "react";
+import coverPhoto from "../../images/cursos/curso-en-video/cover-photo.png";
+import playIcon from "../../images/cursos/play-icon.svg";
+
+const HeaderCurso = () => {
+  return (
+    <div className="section section-purple">
+      <div className="container">
+        <div className="row no-gutters align-items-center">
+          <div className="col-lg-6 p-lg-r">
+            <h2 className="mb-3 text-primary">
+              Curso básico en video para sobrevivir al SAT...anas 👹
+            </h2>
+            <img
+              src={coverPhoto}
+              className="img-fluid mb-4 d-lg-none"
+              style={{ borderRadius: 24, width: "100%" }}
+            />
+            <p className="text-light">
+              En este curso online <span className="text-primary">*</span> aprenderás lo más básico que necesitas saber para
+              darte de alta y comenzar con tu vida fiscal
+            </p>
+            <div className="mt-2 mb-4">
+              <p className="font-xs text-light">
+                <span className="text-primary">* Pago 100% seguro, recibes acceso inmediato a los videos en alta calidad para siempre</span>
+              </p>
+            </div>
+            <div className="d-flex flex-column flex-md-row">
+              <a
+                className="btn btn-primary btn-lg mb-3 mr-3"
+                style={{ width: "100%" }}
+                href="https://lahorasat.podia.com/curso-en-video"
+                data-podia-embed="link"
+              >
+                Pagar en linea
+              </a>
+              <a
+                className="btn btn-outline-primary btn-lg mb-3"
+                style={{ width: "100%" }}
+                target="_blank"
+                href="https://wa.me/+525510722129"
+              >
+                Agendar por Whatsapp
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-6 d-none d-lg-block">
+            <a href="#" className="video-link-icon">
+              <img
+                src={coverPhoto}
+                className="img-fluid mb-4"
+                style={{ borderRadius: 24, width: "100%" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCurso;
--- src/pages/curso-en-video.js
+++ src/pages/curso-en-video.js
@@ -1,18 +1,103 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import BannerCursos from "../components/banner-cursos";
 import Asesorias from "../components/asesorias";
+import BulletsCurso from "../components/curso/bullets-curso";
+import HeaderCurso from "../components/curso/header-curso";
+
+const cursoBullets = [
+  "7 lecciones para dejar de malviajarte con el SAT",
+  "Acceso ilimitado para siempre",
+  "Online y a tu propio ritmo",
+  "Material extra de apoyo",
+  "Constancia de participación",
+  "Bloopers de la grabación 😝",
+];
 
 // markup
 const CursoEnVideoPage = () => {
   return (
     <>
       <Layout>
-        <div className="section section-purple">
+        <HeaderCurso />
+        <BulletsCurso bullets={cursoBullets} />
+        <div className="section section-white">
           <div className="container">
-            <BannerCursos />
+            <div className="row">
+              <div className="col-lg-6 pr-5">
+                <h3 className="mb-4">¿Qué aprenderás aquí?</h3>
+                <ul>
+                  <li className="boxed-bullet light">Cómo incorporarte de forma correcta</li>
+                  <li className="boxed-bullet light">Primeros pasos para la vida fiscal</li>
+                  <li className="boxed-bullet light">Tus obligaciones y derechos ante el SAT</li>
+                  <li className="boxed-bullet light">Tips para llevar tu contabilidad de manera sencilla</li>
+                  <li className="boxed-bullet light">Paz mental de saber cómo lidiar con el SAT. 🤓</li>
+                </ul>
+                <p className="mt-3 font-sm">
+                  ¿Tienes dudas?{" "}
+                  <a
+                    target="_blank"
+                    href="https://wa.me/+525510722129"
+                    className="text-underline"
+                  >
+                    Contáctanos por Whats
+                  </a>
+                </p>
+              </div>
+              <div className="col-lg-6">
+                <div className="boxed-bullet p-4">
+                  <h4>Curso básico en video para sobrevivir al SAT...anas 👹</h4>
+                  <div className="d-flex py-3">
+                    <p className="label label-primary mr-2">$299 pesos</p>
+                    <p className="label label-primary">Online</p>
+                  </div>
+                  <p className="font-xs text-secondary mb-3">* Pago 100% seguro, recibes acceso inmediato a los videos en alta calidad para siempre</p>
+                  <a
+                    className="btn btn-secondary mb-3 mr-3 btn-lg"
+                    style={{ width: "100%" }}
+                    href="https://lahorasat.podia.com/curso-en-video"
+                    data-podia-embed="link"
+                  >
+                    Pagar en linea
+                  </a>
+                  <a
+                    className="btn btn-outline-primary btn-lg mb-3"
+                    style={{ width: "100%" }}
+                    target="_blank"
+                    href="https://wa.me/+525510722129"
+                  >
+                    Agendar por Whatsapp
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <ul className="mb-3">
+          <li className="text-light font-sm mb-1">
+            Cómo incorporarte de forma correcta <i>(y que te convenga)</i> ante
+            el SAT{" "}
+          </li>
+          <li className="text-light font-sm mb-1">
+            Primeros pasos para la vida fiscal, cómo tus obligaciones y derechos
+          </li>
+          <li className="text-light font-sm mb-1">
+            Super tips para llevar tu contabilidad de manera sencilla
+          </li>
+        </ul>
+        <p className="text-light text-primary">
+          <strong>Este curso es para ti si...</strong>
+        </p>
+        <ul className="mb-4">
+          <li class="font-sm mb-1">
+            Acabas de salir de la carrera y no sabes como darte de alta
+          </li>
+          <li class="font-sm mb-1">
+            Eres asalariadx pero quieres aprender a sobrevivir ante el SATanas
+          </li>
+          <li class="font-sm mb-1">
+            Te acabas de incorporar y no sabes cual es el siguiente paso
+          </li>
+        </ul>
         <Asesorias />
       </Layout>
     </>
